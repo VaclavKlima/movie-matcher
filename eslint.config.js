982a1171ed09c5f9f156e4a4d1e9adb74f397c46
Dragan <61498435+VaclavKlima@users.noreply.github.com>
2025-12-31 12:01:22 +0100

    Refactor project files for consistent formatting and cleanup
    - Replace double quotes with single quotes for style consistency.
    - Adjust spacing and alignment in CSS, JS, and config files for better readability.
    - Remove unused Prettier check from CI workflow.
    - Simplify `vite.config.js` and `eslint.config.js`.

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,32 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
 
 export default [
-  {
-    ignores: [
-      "bootstrap/cache/**",
-      "node_modules/**",
-      "public/build/**",
-      "storage/**",
-      "vendor/**",
-    ],
-  },
-  js.configs.recommended,
-  {
-    files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        Alpine: "readonly",
-        document: "readonly",
-        navigator: "readonly",
-        requestAnimationFrame: "readonly",
-        setTimeout: "readonly",
-        window: "readonly",
-      },
+    {
+        ignores: [
+            'bootstrap/cache/**',
+            'node_modules/**',
+            'public/build/**',
+            'storage/**',
+            'vendor/**',
+        ],
     },
-    rules: {
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    js.configs.recommended,
+    {
+        files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                Alpine: 'readonly',
+                document: 'readonly',
+                navigator: 'readonly',
+                requestAnimationFrame: 'readonly',
+                setTimeout: 'readonly',
+                window: 'readonly',
+            },
+        },
+        rules: {
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        },
     },
-  },
 ];
