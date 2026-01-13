2d33467f631013cd88f01f6a62c8fe28e7dfbda6
Dragan <61498435+VaclavKlima@users.noreply.github.com>
2026-01-13 20:27:45 +0100

    feat(tmdb): adjust default min popularity threshold to 0.4

--- config/tmdb.php
+++ config/tmdb.php
@@ -3,5 +3,5 @@
 return [
     'read_access_token' => env('TMDB_API_READ_ACCESS_TOKEN'),
     'api_key' => env('TMDB_API_KEY'),
-    'min_popularity' => (float) env('TMDB_MIN_POPULARITY', 0.5),
+    'min_popularity' => (float) env('TMDB_MIN_POPULARITY', 0.4),
 ];
